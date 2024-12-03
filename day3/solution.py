30889e4f692bd1d915da14e785fa3f35d9ee31b2
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2024-12-03 13:30:12 -0500

    Minor update to day 3

--- day3/solution.py
+++ day3/solution.py
@@ -3,20 +3,21 @@ import re
 with open("data.txt", "r") as f:
     raw = f.read()
 
+MULT_EXPR = "mul+\(+\d+\,+\d+\)"
+OPER_EXPR = "((don+\'+t|do)+\(+\))|(mul+\(+\d+\,+\d+\))"
+
 def process_mul(expr) -> int:
     return eval(expr.replace("mul", "").replace("(", "").replace(")", "").replace(",", "*"))
 
 def part1() -> int:
-    expr = "mul+\(+\d+\,+\d+\)"
-    operations = re.findall(expr, raw)
+    operations = re.findall(MULT_EXPR, raw)
     sum = 0
     for oper in operations:
         sum += process_mul(oper)
     return sum
 
 def part2() -> int:
-    expr = "((don+\'+t|do)+\(+\))|(mul+\(+\d+\,+\d+\))"
-    operations = re.findall(expr, raw)
+    operations = re.findall(OPER_EXPR, raw)
     sum = 0
     enabled = True
     for oper in operations:
@@ -25,7 +26,7 @@ def part2() -> int:
         elif oper[0] == "don't()":
             enabled = False
         
-        if enabled and re.search("mul+\(+\d+\,+\d+\)", oper[2]):
+        if enabled and re.search(MULT_EXPR, oper[2]):
             sum += process_mul(oper[2])
 
     return sum
