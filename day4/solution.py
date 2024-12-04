3d50c3af1e9ac598edc8cd4714e8c5d2f81e88c6
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2024-12-04 13:37:17 -0500

    optimized d4

--- day4/solution.py
+++ day4/solution.py
@@ -4,11 +4,21 @@ with open(f"data/data.txt", "r") as f:
 matrix = [i.strip() for i in raw]
 
 class bcolors:
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
     RED = '\033[91m'
     ENDC = '\033[0m'
 
+def print_matrix():
+    for i in range(MAT_WIDTH):
+        row = ""
+        for j in range(MAT_HEIGHT):
+            
+            if (i,j) in to_print:
+                row += f"{getattr(bcolors, to_print[(i,j)])}{matrix[i][j]}{bcolors.ENDC}"
+            else:
+                row += f"{matrix[i][j]}"
+        print(row)
 
 #################### PART 1 ############################
 WORD = "XMAS"
@@ -17,9 +27,7 @@ MAT_WIDTH = len(matrix[0])
 CURRENT_WORD = ""
 
 found_indexes = []
-found_s = []
-found_a = []
-found_m = []
+to_print = {}
 found_words = []
 
 def get_dict(i,j):
@@ -46,8 +54,7 @@ def next_char(char):
 
 def look_direction(i, j, char, direction):
     global found_indexes
-    global found_s
-    global found_a
+    global to_print
     global CURRENT_WORD
 
     n_char = next_char(char)
@@ -71,7 +78,7 @@ def look_direction(i, j, char, direction):
             found_words.append(CURRENT_WORD)
             print("found")
             found = f"{near_item_ij}{direction}{CURRENT_WORD}"
-            found_s.append(near_item_ij)
+            to_print[near_item_ij] = "RED"
             if found not in found_indexes:
                 found_indexes.append(found)
             return True
@@ -79,13 +86,12 @@ def look_direction(i, j, char, direction):
             CURRENT_WORD += n_char
             r = look_direction(near_item_ij[0], near_item_ij[1], n_char, direction)
             if r is True:
-                found_a.append(near_item_ij)
+                to_print[near_item_ij] = "GREEN"
                 return True
 
 
 def look_around(i, j, char):
     global CURRENT_WORD
-    global found_m
 
     around = get_dict(i,j)
               
@@ -96,27 +102,14 @@ def look_around(i, j, char):
             CURRENT_WORD += char
             r = look_direction(loc[0], loc[1], char, direction)
             if r is True:
-                found_m.append(loc)
+                to_print[loc] = "YELLOW"
             
     return False
-
-def print_matrix():
-    for i in range(MAT_WIDTH):
-        row = ""
-        for j in range(MAT_HEIGHT):
-            
-            if (i,j) in found_s:
-                row += f"{bcolors.RED}{matrix[i][j]}{bcolors.ENDC}"
-            elif (i,j) in found_a:
-                row += f"{bcolors.OKGREEN}{matrix[i][j]}{bcolors.ENDC}"
-            elif (i,j) in found_m:
-                row += f"{bcolors.WARNING}{matrix[i][j]}{bcolors.ENDC}"
-            else:
-                row += f"{matrix[i][j]}"
-        print(row)
     
 def part1():
     global CURRENT_WORD
+    global to_print
+    to_print = {}
     for i in range(MAT_WIDTH):
         for j in range(MAT_HEIGHT):
             cur_char = matrix[i][j]
@@ -138,22 +131,13 @@ def check_x(a,b,c,d, char1, char2):
     validation = bound_check(a,b,c,d) and matrix[a][b] == char1 and matrix[c][d] == char2
     if validation:
         global xmas_couples
-        xmas_couples.append((a,b))
-        xmas_couples.append((c,d))
+        to_print[(a,b)] = "RED"
+        to_print[(c,d)] = "RED"
     return validation
 
-def print_matrix_p2():
-    for i in range(MAT_WIDTH):
-        row = ""
-        for j in range(MAT_HEIGHT):
-            
-            if (i,j) in xmas_couples:
-                row += f"{bcolors.RED}{matrix[i][j]}{bcolors.ENDC}"
-            else:
-                row += f"{matrix[i][j]}"
-        print(row)
-
 def part2():
+    global to_print
+    to_print = {}
     count = 0
     for i in range(MAT_WIDTH):
         for j in range(MAT_HEIGHT):
@@ -166,12 +150,10 @@ def part2():
 
                 if (check_x(a,b,c,d,"M","S") or check_x(a,b,c,d,"S","M")) and \
                     (check_x(a,d,c,b,"M","S") or check_x(a,d,c,b,"S","M")):
-                    xmas_couples.append((i,j))
+                    to_print[(i,j)] = "RED"
                     count += 1
-    print_matrix_p2()
+    print_matrix()
     print(count)
-
-
 
 
 
