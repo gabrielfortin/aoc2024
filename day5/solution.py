2288cb4260cc07215af49ff4dd3a24bf953b2821
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2024-12-05 09:45:44 -0500

    d5

---
+++ day5/solution.py
@@ -0,0 +1,68 @@
+import functools
+with open("data/data.txt", "r") as f:
+    raw = f.read()
+
+data1 = raw.split("\n\n")[0]
+data2 = raw.split("\n\n")[1]
+data2 = [[int(i) for i in entry.split(",")] for entry in data2.split("\n")]
+
+rules = {}
+for entry in data1.split("\n"):
+    key, value = entry.split("|")
+    key = int(key.strip())
+    value = int(value.strip())
+    if key not in rules:
+        rules[key] = [value]
+    else:
+        rules[key].append(value)
+
+
+
+print(rules)
+print(data2)
+
+def validate_num(num: str, line):
+    valid = True
+    if num in rules:
+        for rule in rules[num]:
+            if rule in line:
+                if line.index(rule) < line.index(num):
+                    valid = False
+
+    return valid
+
+import math
+
+def compare(a, b):
+    if a in rules and b in rules[a]:
+        return -1
+    elif a == b:
+        return 0
+    else:
+        return 1
+
+def fix_line(line):
+    newline = sorted(line,key=functools.cmp_to_key(compare))
+    return newline
+
+
+def solve(part: int):
+    sum = 0
+    for line in data2:
+        valid = True
+        for num in line:
+            if not validate_num(num, line):
+                valid = False
+
+        print(valid)
+        if valid and part == 1:
+            middle = line[math.floor(len(line)/2)]
+            sum += middle
+        elif valid is False and part == 2:
+            l = fix_line(line)
+            middle = l[math.floor(len(line)/2)]
+            sum += middle
+    print(sum)
+
+solve(1)
+solve(2)
