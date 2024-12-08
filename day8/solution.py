3c4c1bdf8f05b8dfeb1cb0608a927a6b39177d55
Gab Fortin <gabfortin@icloud.com>
2024-12-08 01:02:38 -0500

    clinupe

--- day8/solution.py
+++ day8/solution.py
@@ -1,25 +1,34 @@
 from copy import deepcopy
 from pprint import pprint
+from typing import Tuple, Optional, List
+
+HARMONIES_CALC_LIMIT = 50
+
+# Parse data
 with open("data/data.txt", "r") as f:
     raw = f.readlines()
 raw = [i.strip() for i in raw]
 matrix = [[j for j in i] for i in raw]
-#pprint(matrix)
 
-antennas_ij = {}
+# Identify antennas
 antennas_types = {}
 for i in range(len(matrix)):
     for j in range(len(matrix[i])):
         elem = matrix[i][j]
         pos_str = f"{i},{j}"
         if elem != ".":
-            antennas_ij[pos_str] = elem
             if elem in antennas_types:
                 antennas_types[elem].append((i,j))
             else:
                 antennas_types[elem] = [(i,j)]
 
-def calc_harmonies(pos1, pos2, p1=True):
+def calc_harmonies(pos1: Tuple[int, int], pos2: Tuple[int, int], p1: Optional[bool]=True) -> List[Tuple[int,int]]:
+    """
+    calculate harmonies
+    :param pos1: position of the first antenna
+    :param pos2: position of the first antenna
+    :param p1: If true, will calculate for part 1 of day 8, otherwise, for part 2
+    """
     delta_x = int(pos1[0]) - int(pos2[0])
     delta_y = int(pos1[1]) - int(pos2[1])
 
@@ -28,20 +37,14 @@ def calc_harmonies(pos1, pos2, p1=True):
                 (pos2[0]-delta_x, pos2[1]-delta_y)]
     else:
         hs = []
-        base_posx1 = pos1[0]
-        base_posy1 = pos1[1]
-        base_posx2 = pos2[0]
-        base_posy2 = pos2[1]
-        for i in range(500):
-            hs.append((base_posx1+delta_x, base_posy1+delta_y))
-            hs.append((base_posx2-delta_x, base_posy2-delta_y))
-            base_posx1+=delta_x
-            base_posy1+=delta_y
-            base_posx2-=delta_x
-            base_posy2-=delta_y
+        p_vector = [pos1[0], pos1[1], pos2[0], pos2[1]]
+        d_vector = [delta_x, delta_y, 0-delta_x, 0-delta_y]
+        for i in range(HARMONIES_CALC_LIMIT):
+            hs.extend([(p_vector[0]+delta_x, p_vector[1]+delta_y), (p_vector[2]-delta_x, p_vector[3]-delta_y)])
+            p_vector = [a + b for a, b in zip(p_vector, d_vector)]
         return hs
 
-def calc(p1=True):
+def calc(p1: Optional[bool] = True):
     new_mat = deepcopy(matrix)
     an_locs=dict()
 
